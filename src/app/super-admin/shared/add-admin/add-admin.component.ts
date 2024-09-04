4730419c37f1594ab704da26822db0b1c8e31c7d
vasuyadav1220 <mayankyadav1203@gmail.com>
2024-09-04 16:48:19 +0530

    mayank image error commit

--- src/app/super-admin/shared/add-admin/add-admin.component.ts
+++ src/app/super-admin/shared/add-admin/add-admin.component.ts
@@ -30,6 +30,8 @@ export class AddAdminComponent implements OnInit {
   onSubmit() {
     if (this.myForm.valid) {
      this.ck = true;
+     this.myForm.value.image = this.url
+     console.log(this.url)
      this.service.postDoctors(this.myForm.value).subscribe((res:any)=>{
       console.log('form added',res)
      })
@@ -47,5 +49,41 @@ export class AddAdminComponent implements OnInit {
 
     //   this.myForm.markAllAsTouched();
     // }
+  }
+  url: any;
+  fileType!: string;
+
+  onSelectFile(event: any) {
+    let file = event.target.files[0];
+    if (file.type.includes('image')) {
+      this.fileType = 'image';
+    } else if (file.type.includes('pdf')) {
+      this.fileType = 'pdf';
+    } else {
+      this.fileType = ''; // Reset fileType if neither image nor pdf
+    }
+    
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => {
+      this.url = reader.result;
+      
+      this.myForm.value.image = reader.result;
+    };
+    if (event.target.files && event.target.files[0]) {
+      if (
+        event.target.files[0].type === 'image/jpeg' ||
+        event.target.files[0].type === 'image/png' ||
+        event.target.files[0].type === 'image/jpg' ||
+        event.target.files[0].type === 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+      ) {
+        if (event.target.files[0].size < 200 * 200) {
+          /* Checking height * width*/
+        }
+        if (event.target.files[0].size < 20000) {
+          /* checking size here - 2MB */
+        }
+      }
+    }
   }
 }
