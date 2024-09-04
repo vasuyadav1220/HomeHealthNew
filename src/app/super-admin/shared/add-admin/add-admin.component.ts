02a55aad88ee23bbdd547e1cd1acfbc950901a8a
Gautam bairagi <109797346+GautamBairagi@users.noreply.github.com>
2024-09-04 16:50:10 +0530

    gautam commit

--- src/app/super-admin/shared/add-admin/add-admin.component.ts
+++ src/app/super-admin/shared/add-admin/add-admin.component.ts
@@ -11,7 +11,6 @@ import { AllService } from 'src/app/Api/all.service';
 export class AddAdminComponent implements OnInit {
   myForm!: FormGroup;
   ck:boolean=false;
-
   constructor(private fb: FormBuilder,
     private route:Router,
     private service:AllService
@@ -24,28 +23,25 @@ export class AddAdminComponent implements OnInit {
       password: ['', [Validators.required, Validators.minLength(6)]],
       mobileNumber: ['', [Validators.required, Validators.pattern('^[0-9]{10}$')]],
       image: ['', Validators.required]
+
+
+
+
     });
   }
 
   onSubmit() {
     if (this.myForm.valid) {
      this.ck = true;
-     this.service.postDoctors(this.myForm.value).subscribe((res:any)=>{
+          this.service.postDoctors(this.myForm.value).subscribe((res:any)=>{
       console.log('form added',res)
      })
     return
     }
-    //  else {
-    //     console.log("Pataient data",this.myForm.value)
-    //     this.service.postDoctors(this.myForm.value).subscribe({
-    //       next: (res)=>{
-    //           this.route.navigate(["/superAdmin/view_admins"])
-    //       },
-    //       error: (err)=>{console.log(err)}
-    //       })
 
-
-    //   this.myForm.markAllAsTouched();
-    // }
   }
+
+
+
+
 }
